fd35bf1c1c79fd24dd00be3697e9944c8af5a2c2
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 14:15:43 +0200

    test

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
 from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Config.set('graphics', 'width', '340')
 Config.set('graphics', 'height', '700')
+Builder.load_file('app.kv')
+
 class DescriptionView(BoxLayout):
     pass
 
-class CameraView(BoxLayout):
-    def __init__(self):
-        super(CameraView, self).__init__()
+class CameraView(Screen):
+    def build(self):
+        pass
         
-    
-class Home(BoxLayout):
-    def __init__(self):
-        super(Home, self).__init__()
-    
-    def take_picture(self):
-        return CameraView()
-    
+class Home(Screen):
+        
     def import_picture(self):
         print("picture import")
 
 
 class App(App):
     def build(self):
-        return Home()
+        sm = ScreenManager()
+        sm.add_widget(Home(name='home'))
+        sm.add_widget(CameraView(name='cameraview'))
+        return sm
 
 app =  App()
 app.run()
