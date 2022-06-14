43ad317d3c8f9de1d2d75cb378ada1ae91be4f69
BuildTools <unconfigured@null.spigotmc.org>
2022-06-14 16:47:10 +0200

    commit lfs

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def get_prediction(request : Request):
 
 if __name__ == '__main__':
 
-    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
+    uvicorn.run(app, host='localhost', debug=True)
